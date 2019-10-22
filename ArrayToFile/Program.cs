0bade40a112379252dd1506f5d80c57bd06a7e5a
Ivo Oude Veldhuis <ivo@oudeveldhuis.net>
2019-10-22 19:32:32 +0200

    Add project files.

---
+++ ArrayToFile/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace ArrayToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myArrayList = new List<string>() { "week1Box", "week2Box", "week3Box", "week4Box", "week5Box", "week6Box", "week7Box", "week8Box", "week9Box", "week10Box" };
+            File.WriteAllLines("output.txt", myArrayList);
+        }
+    }
+}
